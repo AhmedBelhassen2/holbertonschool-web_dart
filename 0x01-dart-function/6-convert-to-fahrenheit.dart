c32e3574d8bf7219ad93f037c61d10619b14ccca
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2023-02-15 17:35:15 +0100

    6-convert-to-fahrenheit.dart

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,8 +1,8 @@
-List<String> convertToF(List<double> tempInC) {
+List<double> convertToF(List<double> temperaturesInC) {
     
     var idx = 0;
-    List<String> list = [];
+    List<double> list = [];
    
-    for (; idx < tempInC.length; idx++) { list.add((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2)); }
+    for (idx = 0 ; idx < temperaturesInC.length; idx++) { list.add((double.parse((temperaturesInC[idx] * 9 / 5 + 32).toStringAsFixed(2)))); }
     return list;
 }
