658ec4e976428e92e92beb806a9a709964421e67
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2023-02-15 17:29:49 +0100

    6-convert-to-fahrenheit.dart

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -3,6 +3,6 @@ List<String> convertToF(List<double> tempInC) {
     var idx = 0;
     List<String> list = [];
    
-    for (idx = 0 ; idx < tempInC.length; idx++) { list.add((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2)); }
+    for (; idx < tempInC.length; idx++) { list.add((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2)); }
     return list;
 }
