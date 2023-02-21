99539791b6bd2ae8e216e7a750a15080078694c2
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2023-02-21 01:47:27 +0100

    class

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,7 +1,10 @@
 class Password {
-  String _password;
+ String _password;
   Password({required String password}) : _password = password;
   String get password => _password;
+  set password(String password) {
+    _password = password;
+  }
   bool isValid() {
     return ((password.length >= 6) && (password.length <= 18)) && this.password.contains(new RegExp(r"[A-Z]")) && this.password.contains(new RegExp(r"[a-z]")) && this.password.contains(new RegExp(r"[0-9]"));
   }
