41aef3fe1384fdbfc9fc176c77c7ee179081ae0f
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2023-02-21 02:35:38 +0100

    rick_and_morty.dart

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,14 +1,15 @@
 import "package:http/http.dart" as http;
-import 'dart:convert';
+import 'dart:convert' as convert;
 
 printRmCharacters() async {
+  var url = Uri.https("rickandmortyapi.com", "/api/character/");
   try {
-    final res =  await http.get(Uri.parse('https://rickandmortyapi.com/api/character'),
-    );
-    var json = jsonDecode(res.body);
-
-    for (var idx = 0; idx < json.length; idx++) {
-      print("${json[idx]['name']}");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = convert.jsonDecode(response.body);
+      for (int i = 0; i < jsonResponse.length; i++) {
+        print(jsonResponse[i]['name']);
+      }
     }
   } catch (error) {
     return "error caught: ${error}";
